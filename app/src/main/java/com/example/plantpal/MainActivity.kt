28216c5896e7f873b50702fa90d24e4f58caa7ac
samantha <samanthap8675@gmail.com>
2026-04-07 17:43:02 -0400

    added fixes

--- app/src/main/java/com/example/plantpal/MainActivity.kt
+++ app/src/main/java/com/example/plantpal/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             PlantPalTheme {
-                PlantPalApp()
+                PlantPalApp() 
             }
         }
     }
